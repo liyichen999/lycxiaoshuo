191f43c7655441774c08df25a9e4f2144cc1e5a1
liyichen999 <18631167199@163.com>
2026-06-22 19:44:34 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,11 @@
-const CACHE_NAME = 'lyc-shuku-cache-v1';
-const ASSETS_TO_CACHE = [
-  './11111.html',
-  './manifest.json',
-  './service-worker.js',
-  './icon.svg'
-];
-
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => Promise.all(
-      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-    ))
-  );
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) return cachedResponse;
-      return fetch(event.request).then((response) => {
-        if (!response || response.status !== 200 || response.type !== 'basic') return response;
-        const responseClone = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
-        return response;
-      });
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
